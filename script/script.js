27de15b0014ade3baac94c75e9a564e2927c3895
Abdus-Samad Charles <a.charles.edu@gmail.com>
2022-06-18 13:08:38 +0200

    IP

---
+++ script/script.js
@@ -0,0 +1,25 @@
+
+const base = document.getElementById('base');
+const height = document.getElementById('height');
+const area = document.getElementById('area');
+
+const btnCalc = document.getElementById('calculate');
+btnCalc.addEventListener('click', (b, h) => {
+    b = parseFloat(base.value);
+    h = parseFloat(height.value);
+    area.innerHTML = (0.5 * b * h).toFixed(2);
+})
+
+const btnClear = document.getElementById('clear');
+btnClear.addEventListener('click', () => {
+    base.value = '';
+    height.value = '';
+    area.innerHTML = '';
+})
+
+
+// copyright
+const Year = new Date().getFullYear(); 
+document.getElementById('footer').innerHTML = `
+&copyAbdus-Samad Charles ${Year}
+`;
